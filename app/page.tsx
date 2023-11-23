1e1efa3359628498a4b3519889e30568dd39eb47
Andreas Remdt <me@andreasremdt.com>
2023-11-23 17:55:11 +0100

    feat: add filter function

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,8 @@ import expenses from "@/data.json";
 
 export default function Page() {
   return (
-    <>
-      <header className="flex items-center justify-between">
-        <h1 className="text-3xl text-gray-900 font-bold mb-6">Expenses</h1>
-        <Button>Add Expense</Button>
-      </header>
-
-      <Table expenses={expenses} />
-    </>
+    <Table title="Expenses" expenses={expenses}>
+      <Button>Add expense</Button>
+    </Table>
   );
 }
--- lib/utils.ts
+++ lib/utils.ts
@@ -11,7 +11,19 @@ export function formatDate(value: string) {
   return new Intl.DateTimeFormat("de-DE").format(new Date(value));
 }
 
-export function groupExpensesByDate(expenses: Expense[]) {
+export function filterExpenses(expenses: Expense[], filter: string) {
+  let lowerCaseFilter = filter.toLowerCase();
+
+  return expenses.filter(({ title, category, value }) => {
+    return (
+      title.toLowerCase().includes(lowerCaseFilter) ||
+      category.toLowerCase().includes(lowerCaseFilter) ||
+      String(value).toLowerCase().includes(lowerCaseFilter)
+    );
+  });
+}
+
+export function groupExpenses(expenses: Expense[]) {
   let grouped = new Map<string, Expense[]>();
 
   for (let expense of expenses) {
