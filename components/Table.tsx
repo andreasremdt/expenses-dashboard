c5820398609447035de15bccfd55941665fc3081
Andreas Remdt <me@andreasremdt.com>
2023-11-23 15:50:25 +0100

    feat: add grouping by date

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import { formatCurrency, formatDate } from "@/lib/utils";
+import { formatCurrency, formatDate, groupExpensesByDate } from "@/lib/utils";
 import type { Expense } from "@/types";
 import Button from "@/components/button";
+import { Fragment } from "react";
 
 type Props = {
   expenses: Expense[];
@@ -32,22 +33,31 @@ export default function Table({ expenses }: Props) {
         </tr>
       </thead>
       <tbody>
-        {expenses.map((expense) => (
-          <tr key={expense.id}>
-            <td className="border-b border-gray-200 p-2">
-              <input type="checkbox" />
-            </td>
-            <td className="border-b border-gray-200 p-2 font-medium">{expense.title}</td>
-            <td className="border-b border-gray-200 p-2">{formatDate(expense.created)}</td>
-            <td className="border-b border-gray-200 p-2">{formatCurrency(expense.value)}</td>
-            <td className="border-b border-gray-200 p-2">{expense.category}</td>
-            <td className="border-b border-gray-200 p-2">
-              <Button variant="secondary" className="mr-2">
-                Edit
-              </Button>
-              <Button variant="secondary">Delete</Button>
-            </td>
-          </tr>
+        {Array.from(groupExpensesByDate(expenses)).map(([date, expenses]) => (
+          <Fragment key={date}>
+            <tr>
+              <td colSpan={6} className="bg-gray-50 border-b border-gray-300 py-2 px-10 font-semibold text-gray-900">
+                {formatDate(date)}
+              </td>
+            </tr>
+            {expenses.map((expense) => (
+              <tr key={expense.id}>
+                <td className="border-b border-gray-200 p-2">
+                  <input type="checkbox" />
+                </td>
+                <td className="border-b border-gray-200 p-2 font-medium">{expense.title}</td>
+                <td className="border-b border-gray-200 p-2">{formatDate(expense.created)}</td>
+                <td className="border-b border-gray-200 p-2">{formatCurrency(expense.value)}</td>
+                <td className="border-b border-gray-200 p-2">{expense.category}</td>
+                <td className="border-b border-gray-200 p-2">
+                  <Button variant="secondary" className="mr-2">
+                    Edit
+                  </Button>
+                  <Button variant="secondary">Delete</Button>
+                </td>
+              </tr>
+            ))}
+          </Fragment>
         ))}
       </tbody>
     </table>
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,5 @@
+import type { Expense } from "@/types";
+
 export function formatCurrency(value: number) {
   return new Intl.NumberFormat("de-DE", {
     style: "currency",
@@ -7,4 +9,22 @@ export function formatCurrency(value: number) {
 
 export function formatDate(value: string) {
   return new Intl.DateTimeFormat("de-DE").format(new Date(value));
+}
+
+export function groupExpensesByDate(expenses: Expense[]) {
+  let grouped = new Map<string, Expense[]>();
+
+  for (let expense of expenses) {
+    let created = expense.created.slice(0, 10);
+
+    if (grouped.has(created)) {
+      let previous = grouped.get(created) as Expense[];
+
+      grouped.set(created, [...previous, expense]);
+    } else {
+      grouped.set(created, [expense]);
+    }
+  }
+
+  return grouped;
 }
